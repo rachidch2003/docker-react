2ce4d64a934d1f06108e237e243d466b436db1e5
root <root@hermodbdf.bdf.local>
2020-11-16 10:01:05 +0100

    fix test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-         Hi Guys really
+         Hi Guys really /learn react/i
         </a>
       </header>
     </div>
